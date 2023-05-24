9ad6e8f685fa20753866518718223559113a77ca
sgrazys <s.grazys@gmail.com>
2023-05-24 13:59:58 +0300

    Geolocation #3

--- script.js
+++ script.js
@@ -15,15 +15,15 @@ const inputElevation = document.querySelector('.form__input--elevation');
 if (navigator.geolocation)
 	navigator.geolocation.getCurrentPosition(
 		function (position) {
-			console.log(position);
 			const { latitude } = position.coords;
 			const { longitude } = position.coords;
-			console.log(
-				`https://www.google.com/maps/@${latitude},${longitude}`
-			);
+			// console.log(
+			// 	`https://www.google.com/maps/@${latitude},${longitude}`
+			// );
 
 			const coords = [latitude, longitude];
 			const map = L.map('map').setView(coords, 13);
+			// console.log(map);
 
 			L.tileLayer(
 				'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
@@ -33,10 +33,23 @@ if (navigator.geolocation)
 				}
 			).addTo(map);
 
-			L.marker(coords)
-				.addTo(map)
-				.bindPopup('A pretty CSS popup.<br> Easily customizable.')
-				.openPopup();
+			map.on('click', function (mapEvent) {
+				const { lat, lng } = mapEvent.latlng;
+
+				L.marker([lat, lng])
+					.addTo(map)
+					.bindPopup(
+						L.popup({
+							maxWidth: '250',
+							minWidth: '100',
+							autoClose: false,
+							closeOnClick: false,
+							className: 'running-popup',
+						})
+					)
+					.setPopupContent('Workout')
+					.openPopup();
+			});
 		},
 		function () {
 			alert('Could not get your position.');
